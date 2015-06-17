cef4d82f2f7cd43df2bbd8e760ce13af4667e5cb
tokue_t_alps <tokue@alpsgiken.co.jp>
2015-06-17 17:41:56 +0900

    メニュー選択時の表示修正
    メニュー選択時にプロジェクト情報が表示されるよう対応
    メニュー選択時にタブが選択状態となるよう対応

--- app/controllers/delivery_list_controller.rb
+++ app/controllers/delivery_list_controller.rb
@@ -1,6 +1,12 @@
 class DeliveryListController < ApplicationController
   unloadable
 
+  # メニュー選択時にタブが選択状態となるようにするおまじない
+  menu_item :delivery_list
+
+  # プロジェクトメニュー配下にするためのおまじない  (初期処理)
+  # authorizeは非ログイン時はログイン画面へ遷移するおまじない
+  before_filter :find_project, :authorize
 
   def index
   end
@@ -12,5 +18,12 @@ class DeliveryListController < ApplicationController
   end
 
   def edit
+  end
+
+private
+  def find_project
+    @project = Project.find(params[:project_id])
+  rescue ActiveRecord::RecordNotFound
+    render_404
   end
 end
