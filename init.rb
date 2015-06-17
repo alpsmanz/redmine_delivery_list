e6035727ff43e5c7194127a5274bc9a4fbe172dc
tokue_t_alps <tokue@alpsgiken.co.jp>
2015-06-17 16:36:22 +0900

    モジュールの権限設定
    モジュールの権限設定を追加
    データ表示権限：ndex, show
    データの管理：new, edit, create, update, destory, preview

--- init.rb
+++ init.rb
@@ -5,4 +5,11 @@ Redmine::Plugin.register :redmine_delivery_list do
   version '0.0.1'
   url 'http://example.com/path/to/plugin'
   author_url 'http://example.com/about'
+  
+  project_module :delivery_list do
+    permission :view_delivery_list, :delivery_list => [:index, :show]
+    permission :manage_delivery_list, :delivery_list => [:new, :edit, :create, :update, :destroy, :preview],
+    :require => :member
+  end
+  
 end
