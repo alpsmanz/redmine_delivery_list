f4765a274c617b27a771a5a64c73634c08a1fc5a
tokue_t_alps <tokue@alpsgiken.co.jp>
2015-06-17 14:23:36 +0900

    スケルトン作成
    プラグインのスケルトン作成(初回登録)

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_delivery_list do
+  name 'Redmine Delivery List plugin'
+  author 'altec corp'
+  description 'This is a plugin for Redmine to disp Delivery date List'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
